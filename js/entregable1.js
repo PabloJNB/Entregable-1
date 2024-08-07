e20a809457fe220b71b33c78823968784fdc8e0e
PJNB <josecuccia72@gmail.com>
2024-08-06 21:21:27 -0300

    sisi
    :)=

--- js/entregable1.js
+++ js/entregable1.js
@@ -1,14 +1,14 @@
 // Botón de mover a lista finalizada
-const pendientesCbox = document.querySelectorAll(".pendientesCbox");
-const listaFinalizadas = document.querySelector(".lista-terminada");
 
 // Crear un evento para cada checkbox
+const pendientesCbox = document.querySelectorAll(".pendientesCbox");
 pendientesCbox.forEach((checkbox) => {
     checkbox.addEventListener("click", function () {
         if (checkbox.checked) {
             const tarea = checkbox.parentNode;
-
+            
             // Mover la tarea a la lista de finalizadas
+            const listaFinalizadas = document.querySelector(".lista-terminada");
             listaFinalizadas.appendChild(tarea);
             
             // Cambiar la clase del checkbox
